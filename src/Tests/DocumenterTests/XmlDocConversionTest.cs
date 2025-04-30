a3bb1b686e1eaf056a75e90b6b3186f7fef174d6
EvilBeaver <ovsiankin.aa@gmail.com>
2025-04-30 17:46:32 +0300

    Фикс теста лицензий

--- src/Tests/DocumenterTests/XmlDocConversionTest.cs
+++ src/Tests/DocumenterTests/XmlDocConversionTest.cs
@@ -1,3 +1,10 @@
+/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the
+Mozilla Public License, v.2.0. If a copy of the MPL
+was not distributed with this file, You can obtain one
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
 using System.Linq;
 using System.Reflection;
 using System.Xml.Linq;
@@ -7,7 +14,7 @@ using Xunit;
 
 namespace DocumenterTests
 {
-    public class XmlDocConversionText
+    public class XmlDocConversionTest
     {
         [Fact]
         public void TestConversionOfTextBlock()
